a2b2c4d9d74068dc281351b04fd8df70f1f0cb61
Maximo De Leon <maxleonaquino@gmail.com>
2019-04-09 22:22:56 -0700

    about session automated

--- api/controllers/IndexController.js
+++ api/controllers/IndexController.js
@@ -8,7 +8,94 @@
 module.exports = {
     index: function (req, res) {
         let sliderInfo = [{ h1Text: "Amazing Design", h2Text: "Create your dream with", image: "/images/slide/code_alone.jpg" }];
-        return res.view('pages/homepage', { sliderInfo });
+        let team = [
+            {
+                greeting: "Hello!",
+                postGreeting: "Devin Activo post gretting.",
+                name: "Maximo De Leon",
+                role: "CEO",
+                picture: "/images/team/team-1.jpg",
+                socials: [
+                    { account: "blablablafacebook.com", icon: "fa fa-facebook" },
+                    { account: "blablablatwitter.com", icon: "fa fa-twitter" },
+                    { account: "blablablatwitter.com", icon: "fa fa-pinterest" }
+                ]
+            },
+            {
+                greeting: "Hello!",
+                postGreeting: "Devin Activo post gretting.",
+                name: "Eliezer De Leon",
+                role: "CEO",
+                picture: "/images/team/team-1.jpg",
+                socials: [
+                    { account: "blablablafacebook.com", icon: "fa fa-youtube" },
+                    { account: "blablablafacebook.com", icon: "fa fa-facebook" },
+                    { account: "blablablatwitter.com", icon: "fa fa-twitter" },
+                    { account: "blablablatwitter.com", icon: "fa fa-pinterest" }
+                ]
+            },
+            {
+                greeting: "Hello!",
+                postGreeting: "Devin Activo post gretting.",
+                name: "Edwin Frias",
+                role: "CEO",
+                picture: "/images/team/team-1.jpg",
+                socials: [
+                    { account: "blablablafacebook.com", icon: "fa fa-facebook" },
+                    { account: "blablablatwitter.com", icon: "fa fa-twitter" },
+                    { account: "blablablatwitter.com", icon: "fa fa-pinterest" }
+                ]
+            },
+            {
+                greeting: "Hello!",
+                postGreeting: "Devin Activo post gretting.",
+                name: "Manuel Gonzalez",
+                role: "CEO",
+                picture: "/images/team/team-1.jpg",
+                socials: [
+                    { account: "blablablafacebook.com", icon: "fa fa-facebook" },
+                    { account: "blablablatwitter.com", icon: "fa fa-twitter" },
+                    { account: "blablablatwitter.com", icon: "fa fa-pinterest" }
+                ]
+            },
+            {
+                greeting: "Hello!",
+                postGreeting: "Devin Activo post gretting.",
+                name: "Jhonny Gil",
+                role: "CEO",
+                picture: "/images/team/team-1.jpg",
+                socials: [
+                    { account: "blablablafacebook.com", icon: "fa fa-facebook" },
+                    { account: "blablablatwitter.com", icon: "fa fa-twitter" },
+                    { account: "blablablatwitter.com", icon: "fa fa-pinterest" }
+                ]
+            },
+            {
+                greeting: "Hello!",
+                postGreeting: "Devin Activo post gretting.",
+                name: "Jhonny Gil",
+                role: "CEO",
+                picture: "/images/team/team-1.jpg",
+                socials: [
+                    { account: "blablablafacebook.com", icon: "fa fa-facebook" },
+                    { account: "blablablatwitter.com", icon: "fa fa-twitter" },
+                    { account: "blablablatwitter.com", icon: "fa fa-pinterest" }
+                ]
+            },
+            {
+                greeting: "Hello!",
+                postGreeting: "Devin Activo post gretting.",
+                name: "Jhonny Gil",
+                role: "CEO",
+                picture: "/images/team/team-1.jpg",
+                socials: [
+                    { account: "blablablafacebook.com", icon: "fa fa-facebook" },
+                    { account: "blablablatwitter.com", icon: "fa fa-twitter" },
+                    { account: "blablablatwitter.com", icon: "fa fa-pinterest" }
+                ]
+            }
+        ]
+        return res.view('pages/homepage', { sliderInfo, team });
     }
 };
 
