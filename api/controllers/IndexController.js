be9e7b4a45bcdf91c696f1462aa859734025e132
Maximo De Leon <maxleonaquino@gmail.com>
2019-05-31 15:23:05 -0400

    Update IndexController.js

--- api/controllers/IndexController.js
+++ api/controllers/IndexController.js
@@ -47,7 +47,7 @@ module.exports = {
 
         let products = await Products.find();
         
-        console.log(contact);
+        console.log(contact + "test");
       
         return res.view('pages/homepage', { sliderInfo, team, navtabs, portfolio, features, partners, contact, products });
     }
